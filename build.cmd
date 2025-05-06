6e44f726e8d97cbe354cdcb6274d33d977897f20
Zero <gamezerostudio@qq.com>
2025-05-06 22:21:46 +0800

    [feature] 支持Mono编译

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
 @echo off
-scons platform=windows vsproj=yes dev_build=yes architecture=x86_64 target=editor
+scons platform=windows vsproj=yes dev_build=yes architecture=x86_64 target=editor module_mono_enabled=yes
+.\bin\godot.windows.editor.dev.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
+python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir=.\bin
 pause
